5ee7fe1ceda18428e38e59fd3deab042b3c68b68
Derek McDaniel <derek7mc@gmail.com>
2021-06-25 19:48:14 -0600

    edits

--- softlock.js
+++ softlock.js
@@ -16,18 +16,42 @@ function kvrev(){
       let return_revisions = [];
       for(let i=0; i<args.length; ++i){
         let k = args[i];
+        let skip = false;
+        if(k[0] == ":"){
+          skip = true;
+          k = k.substr(1);
+        }
         let v = values[k];
         let r = revisions[k] ?? 0;
 
-        // keys prefixed with "#" return the revision only.
-        return_values.push(v);
+        // keys prefixed with ":" return the revision only.
+        return_values.push(skip? null : v);
         return_revisions.push(r);
       }
       // if(Math.random() < 0.0001) return no("Not today.");
       return yes(return_values, return_revisions);
     });
   }
+  X.set = (locks, ...args) => {
+    return new Promise((yes, no) => {
+      for(let k in locks){
+        let r = revisions[k] ?? 0;
+        if(r != locks[k]){
+          return no(`Outdated key '${k}'`);
+        }
+      }
+      for(let i=0; i<args.length - 1; i+=2){
+        let k = args[i];
+        let v = args[i+1];
+        values[k] = v;
+        revisions[k] = (revisions[k]?? 0) + 1;
+      }
+      return yes(args.length/2);
+    })
+  }
+
   // if the key is a tuple(or has a comma), then the second item is the revision.
+  /*
   X.set = (...args) => {
     return new Promise((yes, no) => {
       let changes = [];
@@ -60,47 +84,102 @@ function kvrev(){
       }
       return yes(changes.length/2);
     });
-  }
+  }*/
   return X;
 }
 
 
 // this is the client/server combo
 function softlock(){
+
   let X = {};
   let __config
   let config_props = {};
   let db = kvrev();
+  
 
-  X.query = function(){
-    let Q = {};
-    let values = {};
-    let revisions = {};
-    let changes = [];
-    let tolock = {};
-
-    Q.ready = false;
-
-    Q.submit = function(){
-      return new Promise((yes, no) => {
-        db.get(
-      });
-    }
-    Q.lock = function(...args){
-      
-      
-    }
-    Q.get = function(){
-    }
-    Q.peek = function(){
-    }
-
-    return Q;
-  }
   X.config = function(k, v){
     if(!(k in config_props){
       return false; }
     __config[k] = v;
+  }
+
+
+  X.query = function(){
+    let Q = {};
+    let values = {};
+    let locked_revisions = {};
+    let getitems = {};
+    let lockitems = {};
+    let peekitems = {};
+    let assign = {};
+
+    Q.ready = false;
+    Q.finished = false;
+    Q.values = {};
+    Q.revisions = {};
+
+    Q.lock = function(...args){
+      for(let i=0; i<args.length; ++i){
+        lockitems[args[i]] = true;
+      }
+    }
+    Q.get = function(...args){
+      for(let i=0; i<args.length; ++i){
+        getitems[args[i]] = true;
+      }
+    }
+    Q.peek = function(...args){
+      for(let i=0; i<args.length; ++i){
+        peekitems[args[i]] = true;
+      }
+    }
+    Q.set = function(...args){
+      for(let i=0; i<args.length - 1; ++i){
+        assign[args[i]] = args[i+1];
+      }
+    }
+
+
+
+    // this is the only promise function in the interface.
+    Q.submit = function(){
+      if(Q.ready){
+        return new Promise((yes, no) => {
+          Q.ready = false;
+          db.set(locked_revisions, ...assign).then({
+            Q.finished = true;
+          });
+        })
+      } else {
+        return new Promise((yes, no) => {
+          let getargs = [];
+          for(let k in getitems){
+            getargs.push(k);
+          }
+          for(let k in peekitems){
+            if(!(k in getitems)) getargs.push(k);
+          }
+          for(let k in lockitems){
+            if(!(k in getitems) && !(k in peekitems)){ 
+              getargs.push(":" + k); }
+          }
+          db.get(getargs).then((items, revs)=>{
+            Q.ready = true;
+            for(let i=0; i<getargs.length; ++i){
+              let key = getargs[i];
+              if(key[0] == ":") key = key.substr(1);
+              let item = items[i];
+              let rev = revs[i];
+              if((key in getitems) || (key in lockitems)){
+                locked_revisions[key] = rev;
+              }
+            }
+          });
+        });
+      }
+    }
+    return Q;
   }
 
   return X;
